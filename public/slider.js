ebeae2f63c6b16dc9734a48f16245ed183c15738
mkolkowski <marius.kolkowski@esemos.de>
2016-03-09 20:43:19 +0100

    - initial commit

--- public/slider.js
+++ public/slider.js
@@ -1,9 +1,9 @@
 define(function (require) {
   require('plugins/kibana-slider-plugin/slider.less');
   require('plugins/kibana-slider-plugin/sliderController');
-  require('ui/registry/vis_types').register(HtmlVisProvider);
+  require('ui/registry/vis_types').register(SliderVisProvider);
 
-  function HtmlVisProvider(Private) {
+  function SliderVisProvider(Private) {
     var TemplateVisType = Private(require('ui/template_vis_type/TemplateVisType'));
 
     return new TemplateVisType({
@@ -13,11 +13,11 @@ define(function (require) {
       description: 'Useful for filtering with sliders in dashboards.',
       template: require('plugins/kibana-slider-plugin/slider.html'),
       params: {
-        editor: require('plugins/kibana-slider-plugin/sliderParams.html')
+        editor: require('plugins/kibana-slider-plugin/sliderOptions.html')
       },
       requiresSearch: false
     });
   }
 
-  return HtmlVisProvider;
+  return SliderVisProvider;
 });
