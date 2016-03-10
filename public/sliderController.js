2677df660f6c0a0941e44847719080fc7e7edd1b
mkolkowski <marius.kolkowski@esemos.de>
2016-03-10 13:42:19 +0100

    - fix update existing filters

--- public/sliderController.js
+++ public/sliderController.js
@@ -4,24 +4,49 @@ define(function (require) {
     var queryFilter = Private(require('ui/filter_bar/query_filter'));
     var buildRangeFilter = require('ui/filter_manager/lib/range');
 
+    $scope.type = 'range';
+    $scope.oldField = null;
+
     $scope.filter = function(field) {
-      if($scope.rangeFilter == null) {
-        $scope.rangeFilter = buildRangeFilter({name: field.id}, {
+      var oldFilter = $scope.findFilter($scope.oldField);
+      if(oldFilter == null) {
+        var rangeFilter = buildRangeFilter({name: field.id}, {
           gte : field.value
         }, $scope.vis.indexPattern);
 
-        queryFilter.addFilters($scope.rangeFilter);
+        queryFilter.addFilters(rangeFilter);
       }
       else {
         $scope.editingFilter = {
-          source: $scope.rangeFilter, // old
+          source: oldFilter, // old
           type: 'range',
-          model: $scope.rangeFilter, // new
-          alias: $scope.rangeFilter.meta.alias
+          model: oldFilter, // new
+          alias: oldFilter.meta.alias
         };
         $scope.editingFilter.model.range.price.gte = $scope.editingFilter.model.range.price.gte + 1;
         queryFilter.updateFilter($scope.editingFilter);
       }
+      $scope.oldField = field;
+    };
+
+    $scope.findFilter = function(oldField) {
+      var foundFilter = null;
+
+      if(oldField != null) {
+        var oldFilter = buildRangeFilter({name: oldField.id}, {
+          gte: oldField.value
+        }, $scope.vis.indexPattern);
+
+        var filters = queryFilter.getFilters();
+        for (var i = 0; i < filters.length; i++) {
+          if (_.isEqual(filters[i][$scope.type], oldFilter[$scope.type])) {
+            foundFilter = filters[i];
+            break;
+          }
+        }
+      }
+
+      return foundFilter;
     };
 
   });
